2252827af897250b93262525c6ecf695370aff99
Harrison <sloab@ust.hk>
2020-07-03 01:36:33 +0800

    update main

--- src_v2/main.py
+++ src_v2/main.py
@@ -1,3 +1,30 @@
 import pygame
 import random
-#############
+
+WIDTH = 480
+HEIGHT = 360
+FPS = 30
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("My Game")
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(BLACK)
+    pygame.display.flip()
+
+pygame.quit()
