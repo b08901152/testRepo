fdafada8446fff81fa384502a3c6774ba714b2cb
Harrison <sloab@ust.hk>
2020-07-03 11:44:07 +0800

    fix bugs

--- src_v2/gameBasic.py
+++ src_v2/gameBasic.py
@@ -1,12 +1,16 @@
 import pygame
 
-
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
 class Player(pygame.sprite.Sprite):
     def __init__(self, name, image, up, down, left, right, attack, change, pick, x, y, w, h, speed=(0, 0)):
         super().__init__()
 
     def draw(self, screen):
-
+        pass
 
 class Bullet(pygame.sprite.Sprite):
 
--- src_v2/main.py
+++ src_v2/main.py
@@ -1,16 +1,10 @@
 import pygame
 import random
-import gameBasic
+from gameBasic import *
 
 WIDTH = 480
 HEIGHT = 360
 FPS = 30
-
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
 
 pygame.init()
 pygame.mixer.init()
